47966102826fba83ae15deb5565ead0f0345eefd
Maestro <volodimirkucak@icloud.com>
2025-11-03 14:49:12 +0200

    Bag fix

--- client.js
+++ client.js
@@ -1,36 +1,46 @@
 const net = require('net');
 const { program } = require('commander');
+const readline = require('readline');
 
 program
     .option('-h, --host [address]', 'адреса сервера', 'localhost') 
     .option('-p, --port [port]', 'порт сервера', '8000')
-    .option('--sn, --servername [name]', 'ім`я', 'undefined_Client'); 
+    .option('--cn, --clientname [name]', 'ім`я', 'undefined_Client'); 
 
 program.parse();
 const options = program.opts();
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 PORT = options.port;
 HOST = options.host;
-servname = options.servername;
+cliename = options.clientname;
+rl.setPrompt(`${cliename} > `);
 
 const client = net.createConnection({port: PORT, host: HOST}, () =>{
 
-    console.log('Я підключився до сервера!');
+    console.log('---Я підключився до сервера!---\n');
+    rl.prompt();
+    data_imput = rl.on('line', (line) => {
 
-    data_imput = process.stdin.on('data', (data) => {
-
-            filterdata=data.toString().trim()
-            messageToSend = `${servname}: ${filterdata}`
-                client.write(messageToSend)       
+            
+            messageToSend = `${cliename}: ${line}`
+                client.write(messageToSend)      
+                rl.prompt(); 
     });
 
 });
 
 client.on('data', (data) => {
 
-    filterdata = data.toString()
-    console.log(filterdata)
+    const message = data.toString().trim();
+    readline.clearLine(process.stdout, 0);
+    readline.cursorTo(process.stdout, 0);
+    console.log(message)
+    rl.prompt();
 });
 
 client.on('end', () => {
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const net = require('net');
 const { program } = require('commander');
+const readline = require('readline');
 
 program
     .option('-h, --host [address]', 'адреса сервера', 'localhost') 
@@ -9,33 +10,43 @@ program
 program.parse();
 const options = program.opts();
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 PORT = options.port;
 HOST = options.host;
 servname = options.servername;
+rl.setPrompt(`${servname} > `);
 
 const server = net.createServer( (socket) => {
 
-    console.log("Хтось під\'єднався")
-    
+    console.log("---Хтось під\'єднався---\n")
+    rl.prompt();
     socket.on('data',(data) => {
-        filterdata=data.toString().trim()
-            console.log(filterdata)
+        filterdata=data.toString().trim();
+        readline.clearLine(process.stdout, 0);
+        readline.cursorTo(process.stdout, 0);
+        console.log(filterdata);
+        rl.prompt();
     });
-    
+
     socket.on('close',(closeconect) => {
-        console.log("З\'єднання втрачено")
+        console.log("\n---З\'єднання втрачено---\n")
     })
     
-   dataImput = process.stdin.on('data', (data) => {
-        filterdata=data.toString().trim()
+   dataImput = rl.on('line', (line) => {
+       
+        filterdata=line.toString().trim()
         const messageToSend = `${servname}: ${filterdata}`;
             socket.write(messageToSend)
+            rl.prompt();
     });
    
 });
 
 
 server.listen(PORT, HOST, () =>{
-    console.log(`Сервер успішно запущено на ${PORT}:${HOST}`)
+    console.log(`\n---Сервер успішно запущено на ${PORT}:${HOST}---\n`)
 });
